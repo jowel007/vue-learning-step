91d5e66c5fac43fe3d630728cff8734d7e8401fe
jowel007 <abuhanifjowel1@gmail.com>
2024-04-29 11:18:35 +0600

    create component parent and child component

---
+++ src/components/Child.vue
@@ -0,0 +1,8 @@
+<template>
+    <h3>This is child Component</h3>
+</template>
+<script>
+export default{
+    name:'ChildComponent',
+}
+</script>
---
+++ src/views/Parent.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>This is Parent COmponent</h2>
+    <ChildComponent />
+</template>
+<script>
+import ChildComponent from "../components/Child.vue"
+export default{
+    name:'Parent',
+    components:{
+        ChildComponent
+    }
+}
+</script>
