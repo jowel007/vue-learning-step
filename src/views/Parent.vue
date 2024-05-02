b1c62ccc51108aa957c2b97a437a614730b63a6c
jowel007 <abuhanifjowel1@gmail.com>
2024-05-02 15:03:48 +0600

    how to define function inside methods and call the method function

--- src/views/Parent.vue
+++ src/views/Parent.vue
@@ -1,5 +1,6 @@
 <template>
     <ChildComponent :childname="parentname" :clickFunction="alertFunction"/>
+    <button v-on:click="incount">click me</button>  
 </template>
 <script>
 import ChildComponent from "../components/Child.vue"
@@ -11,7 +12,8 @@ export default{
     data()
     {
         return{
-            parentname:"learning vue step by step"
+            parentname:"learning vue step by step",
+            count:0
         }  
     },
 
@@ -20,6 +22,10 @@ export default{
             alertFunction()
             {
                 alert("this is alert");
+            },
+            incount(){
+                this.count =10;
+                console.log(this.count);
             }
             
         }
